939d7d55de5ef4e9d22900edece379a7e91f75fe
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-11-30 00:00:06 +0100

    Create fr.lua

---
+++ esx_policeinteractions/locales/fr.lua
@@ -0,0 +1,37 @@
+Locales = {}
+
+Locales['fr'] = {
+    -- Police Interactions
+    ['HandCuff_uncuff'] = 'Menotter / Démenotter',
+    ['FeetCuff_uncuff'] = 'Entraver / Désentraver',
+    ['Darg'] = 'Escorter',
+    ['Sechce'] = 'Fouiller',
+    ['PutInVehiclece'] = 'Mettre dans le véhicule',  
+    ['OutVehiclece'] = 'Sortir du véhicule',
+    ['StopDargging'] = '[G] - Arrêter l\'escorte',
+    ['searching'] = 'Fouille en cours',
+    
+    -- Notifications
+    ['no_player_nearby'] = 'Aucun joueur à proximité',
+    ['no_vehicle_nearby'] = 'Aucun véhicule à proximité',
+    ['no_free_seats'] = 'Aucune place libre dans le véhicule',
+    ['not_police'] = 'Vous n\'êtes pas policier',
+    ['handcuff_applied'] = 'Menottes appliquées',
+    ['handcuff_removed'] = 'Menottes retirées',
+    ['legcuff_applied'] = 'Entraves appliquées',
+    ['legcuff_removed'] = 'Entraves retirées',
+    ['being_escorted'] = 'Vous êtes escorté',
+    ['escort_stopped'] = 'Escorte arrêtée',
+    ['player_searched'] = 'Joueur fouillé',
+    
+    -- Progress Bars
+    ['searching_player'] = 'Fouille du joueur',
+    ['applying_handcuffs'] = 'Pose des menottes',
+    ['removing_handcuffs'] = 'Retrait des menottes',
+    
+    -- Inventory Items
+    ['handcuff_item'] = 'Menottes',
+    ['handcuff_description'] = 'Utilisé pour restreindre les suspects',
+    ['footcuff_item'] = 'Entraves',
+    ['footcuff_description'] = 'Utilisé pour restreindre les jambes des suspects',
+}
