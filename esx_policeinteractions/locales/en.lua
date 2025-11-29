4599550c42a8ceb02b50a1c6467ae80446417418
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-11-29 23:58:54 +0100

    Add English locale for police interactions

---
+++ esx_policeinteractions/locales/en.lua
@@ -0,0 +1,37 @@
+Locales = {}
+
+Locales['en'] = {
+    -- Police Interactions
+    ['HandCuff_uncuff'] = 'Handcuff / Uncuff',
+    ['FeetCuff_uncuff'] = 'Legcuff / Unlegcuff',
+    ['Darg'] = 'Escort',
+    ['Sechce'] = 'Search',
+    ['PutInVehiclece'] = 'Put in Vehicle',  
+    ['OutVehiclece'] = 'Take out Vehicle',
+    ['StopDargging'] = '[G] - Stop Escorting',
+    ['searching'] = 'Searching',
+    
+    -- Notifications
+    ['no_player_nearby'] = 'No player nearby',
+    ['no_vehicle_nearby'] = 'No vehicle nearby',
+    ['no_free_seats'] = 'No free seats in vehicle',
+    ['not_police'] = 'You are not a police officer',
+    ['handcuff_applied'] = 'Handcuffs applied',
+    ['handcuff_removed'] = 'Handcuffs removed',
+    ['legcuff_applied'] = 'Legcuffs applied',
+    ['legcuff_removed'] = 'Legcuffs removed',
+    ['being_escorted'] = 'You are being escorted',
+    ['escort_stopped'] = 'Escort stopped',
+    ['player_searched'] = 'Player searched',
+    
+    -- Progress Bars
+    ['searching_player'] = 'Searching player',
+    ['applying_handcuffs'] = 'Applying handcuffs',
+    ['removing_handcuffs'] = 'Removing handcuffs',
+    
+    -- Inventory Items
+    ['handcuff_item'] = 'Handcuffs',
+    ['handcuff_description'] = 'Used to restrain suspects',
+    ['footcuff_item'] = 'Legcuffs',
+    ['footcuff_description'] = 'Used to restrain suspects legs',
+}
