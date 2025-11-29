364bdb0cea03b53d2ce862e25ff45ce55f0940ac
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-11-29 23:41:09 +0100

    Update fxmanifest.lua

--- esx_policeinteractions/fxmanifest.lua
+++ esx_policeinteractions/fxmanifest.lua
@@ -7,16 +7,12 @@ version '1.0.0'
 
 client_scripts {
     '@ox_lib/init.lua',
-    'locales/*.lua',
-    'config.lua',
     'client.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     '@ox_lib/init.lua',
-    'locales/*.lua',
-    'config.lua',
     'server.lua'
 }
 
